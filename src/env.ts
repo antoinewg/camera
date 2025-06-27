b9f4082bc59a8b68dd6f8e4fdbf19bcd4f8bc383
antoinewg <a.garcia.walecha@gmail.com>
2025-06-27 22:15:07 +0200

    setup better auth with github provider

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 export const env = createEnv({
   server: {
     SERVER_URL: z.string().url().optional(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
   },
 
   /**
---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env";
+import { betterAuth } from "better-auth";
+ 
+export const auth = betterAuth({
+    socialProviders: {
+        github: { 
+            clientId: env.GITHUB_CLIENT_ID, 
+            clientSecret: env.GITHUB_CLIENT_SECRET, 
+        }, 
+    },
+})
