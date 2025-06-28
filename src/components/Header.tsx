775f4914ffc5ecd623b9ff180be8abef0a8e7192
antoinewg <a.garcia.walecha@gmail.com>
2025-06-28 12:13:40 +0200

    remove demo routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,22 +11,6 @@ export default function Header() {
         <div className="px-2 font-bold">
           <Link to="/">Home</Link>
         </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/start/server-funcs">Start - Server Functions</Link>
-        </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/start/api-request">Start - API Request</Link>
-        </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/sentry/testing">Sentry</Link>
-        </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/tanstack-query">TanStack Query</Link>
-        </div>
       </nav>
       
       <div className="flex items-center gap-2">
