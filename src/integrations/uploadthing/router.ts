f58960cb3cc016c7ab5e59fd5312cf6bdb33fd83
antoinewg <a.garcia.walecha@gmail.com>
2025-06-28 13:15:46 +0200

    protect upload router

--- src/integrations/uploadthing/router.ts
+++ src/integrations/uploadthing/router.ts
@@ -1,9 +1,8 @@
+import { getUser } from "@/lib/auth/auth-server";
 import { createUploadthing, UploadThingError } from "uploadthing/server";
 import type { FileRouter } from "uploadthing/server";
 
 const f = createUploadthing();
-
-const auth = (_req: Request) => ({ id: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const uploadRouter = {
@@ -18,12 +17,9 @@ export const uploadRouter = {
       maxFileCount: 1,
     },
   })
-    // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
+      const user = await getUser(req);
 
-      // If you throw, the user will not be able to upload
       if (!user) throw new UploadThingError("Unauthorized");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
