eb1c0d82dbedc18a120b478da2e167b48f100567
antoinewg <a.garcia.walecha@gmail.com>
2025-06-28 11:57:32 +0200

    move lib auth

---
+++ src/lib/auth/auth.ts
@@ -0,0 +1,24 @@
+import { env } from "@/env";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from "../db/schema"
+import { db } from "../db";
+
+export const auth = betterAuth({
+    socialProviders: {
+        github: { 
+            clientId: env.GITHUB_CLIENT_ID!,
+            clientSecret: env.GITHUB_CLIENT_SECRET!,
+        }, 
+    },
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema: {
+            user: schema.user,
+            account: schema.account,
+            session: schema.session,
+            verification: schema.verification,
+        },
+    }),
+    // plugins: [reactStartCookies()]
+})
